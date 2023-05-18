178017abc1984cd3025af4d321cb2a3e6c50e21d
Akash Chavan <akashc@softype.com>
2023-05-18 17:23:22 +0530

    softype mediplum

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Default values work on localhost:3000 and foomedical.com
 // Replace these values with your own values for production
-export const MEDPLUM_PROJECT_ID = 'projectId="7cfa6392-9d79-4738-b560-fa1fc9029c96"';
+export const MEDPLUM_PROJECT_ID = '7cfa6392-9d79-4738-b560-fa1fc9029c96';
 export const MEDPLUM_GOOGLE_CLIENT_ID = '921088377005-3j1sa10vr6hj86jgmdfh2l53v3mp7lfi.apps.googleusercontent.com';
-export const MEDPLUM_RECAPTCHA_SITE_KEY = '6Ldc5M0lAAAAAEMvzUc3JlHltquCnp0r2opVodVO';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfnoRwmAAAAABUmsWD28TiqB2jMC2C9_iT8s2wF';
