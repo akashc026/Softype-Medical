e8558159edb88038e95f21e62851c81cac16f75d
Akash Chavan <akashc@softype.com>
2023-05-18 18:03:16 +0530

    softype mediplum

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // Replace these values with your own values for production
 export const MEDPLUM_PROJECT_ID = '7cfa6392-9d79-4738-b560-fa1fc9029c96';
 export const MEDPLUM_GOOGLE_CLIENT_ID = '679052511930-8dqur4mmg8egbttgos5pmr4ljtf3etbb.apps.googleusercontent.com';
-export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfnoRwmAAAAABUmsWD28TiqB2jMC2C9_iT8s2wF';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LfnoRwmAAAAABIjUOepTUa8EpdbBzuc-coTxJEP';
